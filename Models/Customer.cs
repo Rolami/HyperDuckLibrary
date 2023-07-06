b98a3f25d15bc5f5a112cb45471ff00e72377bc9
Robin <24991308+Rolami@users.noreply.github.com>
2023-07-06 20:39:12 +0200

    Edited json to have proper usernames and not double emails.

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,33 +1,40 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace HyperDuckLibrary.Models
 {
     public class Customer
     {
-            [Key]
-            public int CustomerId { get; set; } = 0;
+        [Key]
+        public int CustomerId { get; set; } = 0;
 
-            [Required]
-            [MaxLength(20)]
-            public string FirstMidName { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string FirstMidName { get; set; }
 
 
-            [Required]
-            [MaxLength(25)]
-            public string LastName { get; set; }
+        [Required]
+        [MaxLength(25)]
+        public string LastName { get; set; }
 
-            [Required]
-            [EmailAddress]
-            [MaxLength(50)]
-            public string Email { get; set; }
+        [NotMapped]
+        [DisplayName("Name")]
+        public string FullName => (FirstMidName + " " + LastName);
 
-            [Required]
-            [MaxLength(15)]
-            public string PhoneNumber { get; set; }
 
-            [Required]
-            public string UserName { get; set; }
+        [Required]
+        [EmailAddress]
+        [MaxLength(50)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(15)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
 
         public virtual ICollection<BorrowList>? BorrowList { get; set; }
     }
